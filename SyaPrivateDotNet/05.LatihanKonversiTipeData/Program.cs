5f4d26a4bba81e24c5574059f7abc509258d3f93
syad20019ti <syad20019ti@student.nurulfikri.ac.id>
2025-08-22 14:17:47 +0700

    latihan 2

---
+++ SyaPrivateDotNet/05.LatihanKonversiTipeData/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/*--------------- LATIHAN 2------------------*/
+Console.WriteLine("LATIHAN 2");
+
+Console.Write("Input Suhu: ");
+
+string textSuhu = Console.ReadLine();
+double suhuDouble = Convert.ToDouble(textSuhu);
+
+int suhuHasil = Convert.ToInt32(suhuDouble); 
+Console.WriteLine(suhuHasil);
