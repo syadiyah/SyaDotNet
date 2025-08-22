8aa70e392a10d9d43242b6e8278ec4cdf1a6f283
syad20019ti <syad20019ti@student.nurulfikri.ac.id>
2025-08-22 14:37:52 +0700

    latihan 3

---
+++ SyaPrivateDotNet/06.LatihanKonversiTipeData3/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/*--------------- LATIHAN 3------------------*/
+Console.WriteLine("LATIHAN 3");
+
+Console.Write("Usia: ");
+
+string textUsia = Console.ReadLine();
+int usia = Convert.ToInt32(textUsia);
+
+bool usiaBool = int.TryParse(textUsia, out usia);
+
+Console.WriteLine($"Usia Anda adalah {usia} tahun ");
+
