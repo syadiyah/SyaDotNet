9d56feb80deab252fc51735a840950426b7951e6
syad20019ti <syad20019ti@student.nurulfikri.ac.id>
2025-08-25 09:32:24 +0700

    LATIHAN 5

---
+++ SyaPrivateDotNet/08.LatihanKonversiTipeData5/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*--------------- LATIHAN 5------------------*/
+Console.WriteLine("LATIHAN 5");
+
+Console.Write("Input Nilai Numerik: ");
+string nilaiNumerik = Console.ReadLine();
+int angka2 = Convert.ToInt32(nilaiNumerik);
+
+Console.Write("Input Karakter: ");
+string txtKarakter = Console.ReadLine();
+char karakter = char.Parse(txtKarakter);
+
+string hasil =angka2 + karakter.ToString();
+
+Console.WriteLine($"output adalah {hasil}");
