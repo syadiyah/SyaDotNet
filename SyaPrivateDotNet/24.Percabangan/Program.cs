ebaa9c89e90c779ae9b3c709e7179bad0cccd428
syad20019ti <syad20019ti@student.nurulfikri.ac.id>
2025-09-01 19:39:16 +0700

    PERCABANGAN LATIHAN 1

---
+++ SyaPrivateDotNet/24.Percabangan/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/*--------------- LATIHAN 1 PERCABANGAN------------------*/
+Console.Write("Input nilai: ");
+int nilai = Convert.ToInt32(Console.ReadLine());
+
+if (nilai >= 90)
+{
+    Console.WriteLine("Predikat A");
+} 
+else if (nilai >= 80 && nilai <= 89)
+{
+    Console.WriteLine("Predikat B");
+}
+else if (nilai >= 70 && nilai <= 79)
+{
+    Console.WriteLine("Predikat c");
+}
+else if (nilai >= 60 && nilai <= 69)
+{
+    Console.WriteLine("Predikat d");
+} 
+else
+{
+    Console.WriteLine("predikat e");
+}
